08da75c5a97be2f984e0ab3b88a11e417e0b73db
Gulzat33 <islamainazik2007@gmail.com>
2024-03-16 10:07:38 +0600

    dz3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+function TodoApp() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: 'Task 1' },
+    { id: 2, text: 'Task 2' },
+    { id: 3, text: 'Task 3' }
+  ]);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [newTaskText, setNewTaskText] = useState('');
+  const [filteredTasks, setFilteredTasks] = useState(tasks);
+  useEffect(() => {
+    const filtered = tasks.filter(task =>
+        task.text.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTasks(filtered);
+  }, [tasks, searchTerm]);
+
+  const handleDelete = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (newTaskText.trim() !== '') {
+      const newTask = { id: Date.now(), text: newTaskText };
+      setTasks([...tasks, newTask]);
+      setNewTaskText('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <input
+            style={{width: '240px', marginBottom: '10px', marginTop: '10px'}}
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleSearch}
+        />
+        <div>
+          <input
+              type="text"
+              placeholder="Add new task..."
+              value={newTaskText}
+              onChange={(event) => setNewTaskText(event.target.value)}
+          />
+          <button onClick={handleAddTask}>Добавить</button>
+        </div>
+        <ul>
+          {filteredTasks.map(task => (
+              <li key={task.id}>
+                {task.text}
+                <button onClick={() => handleDelete(task.id)}>Удалить</button>
+              </li>
+          ))}
+        </ul>
+      </div>
   );
 }
 
-export default App;
+export default TodoApp;
